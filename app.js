e19015729a2faed9913b303220848eac66f9a153
insideadev <insideadev@gmail.com>
2023-04-05 18:12:16 +0700

    Update to MD5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 app.use(express.static("public"));
@@ -15,8 +15,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -33,13 +31,15 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = md5(req.body.password);
+
     User.findOne({
-        email: req.body.username
+        email: username
     })
         .then((result) => {
             if (result) {
-                if (req.body.password === result.password) {
-                    console.log(result);
+                if ( password === result.password) {
                     res.render("secrets");
                 } else {
                     res.render("login");
@@ -58,7 +58,7 @@ app.post("/register", function (req, res) {
     console.log(req.body.password);
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save()
         .then((result) => {
